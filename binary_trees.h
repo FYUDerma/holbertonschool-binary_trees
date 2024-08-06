17f7fa61340235e23b69564e3632b7d1b06135c9
FYUDerma <145096322+FYUDerma@users.noreply.github.com>
2024-08-06 10:11:09 +0200

    making the binary_trees header with all the .h header

---
+++ binary_trees.h
@@ -0,0 +1,11 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* Header Librairies */
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#endif
