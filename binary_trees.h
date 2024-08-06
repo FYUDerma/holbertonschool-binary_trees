913db6d2b4c6a9a90160589b754392f308de043a
FYUDerma <zakaria.12ans@gmail.com>
2024-08-06 11:06:56 +0200

    adding binary_tree_print prototype to link with the header

--- binary_trees.h
+++ binary_trees.h
@@ -37,4 +37,7 @@ typedef struct binary_tree_s heap_t;
 /* Project Prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
+/* Function Prototype */
+void binary_tree_print(const binary_tree_t *);
+
 #endif
